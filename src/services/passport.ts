3f6170b8ed5af0f39aa27be27acacc2a324876a5
Layne Pierce <ppierce273@gmail.com>
2024-06-14 13:33:22 -0400

    Add interface extending Express.Request.SessionData to store Spotify access token, add param passReqToCallback to SpotifyStrategy

--- src/services/passport.ts
+++ src/services/passport.ts
@@ -1,60 +1,74 @@
-import express, { Request } from 'express';
+import { Request } from 'express';
 import passport from 'passport';
+import { SessionData } from 'express-session';
 import {
     Strategy as SpotifyStrategy,
-    StrategyOptionsWithRequest,
     Profile,
     VerifyCallback,
 } from 'passport-spotify';
 import dotenv from 'dotenv';
-import User, {IUser} from '../models/user';
+import User from '../models/user';
 
 dotenv.config();
 
-passport.serializeUser((user: any, done) => {
-    done(null, user.id);
-})
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
 
-// passport.deserializeUser(()) {
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
 
-// }
+export interface SessionDataWithAccessToken extends SessionData {
+    accessToken?: string;
+}
 
 passport.use(
     new SpotifyStrategy(
         {
             /* 
-             * Casting these as strings prevents error "No overload matches
+            Casting these as strings prevents error "No overload matches
            this call" from the SpotifyStrategy type 
            */
             clientID: process.env.CLIENT_ID as string,
             clientSecret: process.env.CLIENT_SECRET as string,
             callbackURL: process.env.CALLBACK_URL as string,
+            passReqToCallback: true, // pass req to callback for saving access token to session
         },
         async (
+            req: Request & { session: SessionDataWithAccessToken },
             accessToken: string,
             refreshToken: string,
             expires_in: number,
             profile: Profile,
             done: VerifyCallback
         ) => {
-            process.nextTick(() => {
-                User.findOne({ user_id: profile.id }).then((currentUser) => {
-                    if (currentUser) {
-                        console.log(`[server]: existing user: ${currentUser}`);
-                    } else {
-                        let user = new User({
-                            user_id: profile.id,
-                        })
-                            .save()
-                            .then(() => {
-                                console.log(
-                                    `[server]: new user created: ${user}`
-                                );
-                                done(null)
-                            });
-                    }
-                    console.log(profile);
-                });
+            /* 
+            Store access token received from Spotify in session data
+             We'll use it to match the user to their Spotify profile information
+             saved in the database
+            */
+            req.session.accessToken = accessToken;
+
+            await User.findOne({
+                access_token: accessToken,
+            }).then((user) => {
+                if (user) {
+                    console.log('existing user: ' + JSON.stringify(profile));
+                    return done(null, profile);
+                } else {
+                    new User({
+                        access_token: req.session.accessToken,
+                        user_id: profile.id,
+                    })
+                        .save()
+                        .then((user) => {
+                            console.log('new user saved to database:');
+                            return done(null, profile);
+                        });
+                }
             });
         }
     )
