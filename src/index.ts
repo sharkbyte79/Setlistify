df6d55cd5bd759fd96fd6cecc724dc86d3717bc6
Layne Pierce <ppierce273@gmail.com>
2024-06-14 13:34:29 -0400

    Replace cookie session with express-session

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,37 @@
-import express, { Express, Request, Response } from 'express';
-import passport from 'passport';
-import session from 'express-session'
+import express, { Express, Request, Response, Router } from 'express';
+import session from 'express-session';
+import passport, { AuthenticateOptions } from 'passport';
 import mongoose, { ConnectOptions } from 'mongoose';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import { routes } from './routes';
+import './services/passport';
 
 dotenv.config();
 
 const app: Express = express();
 const port: string | number = process.env.PORT || 3000; // default to port 3000
 
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET!,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-// if (process.env.MONDODB_URI) {
+app.use(routes);
+// app.use(authRoute);
+// if (!process.env.MONDODB_URI) {
 //     throw new Error("MongoDB URI couldn't be found!")
-// } 
+// }
 
 /*
 Default to empty string if the MongoDB envar can't be found,
@@ -26,14 +40,16 @@ catch the error during attempt to connect.
 const mongo_uri: string = process.env.MONGODB_URI ?? '';
 mongoose
     .connect(mongo_uri, {
+        /* 
+        These settings will be deprecated in later versions of mongoose
+        */
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        // useCreateIndex: true,
-    } as ConnectOptions).then(() => console.log('[server]: Connected to MongoDB'))
-    .catch((err) =>
-        console.error('[server]: Could not connect to MongoDB:', err)
-);
-
+    } as ConnectOptions)
+    .then(() => console.log('[server]: Connected to MongoDB'))
+    .catch((err) => {
+        throw new Error('[server]: Could not connect to MongoDB\n' + err);
+    });
 
 app.get('/', (req: Request, res: Response) => {
     res.send('TypeScript says Hello, World!');
